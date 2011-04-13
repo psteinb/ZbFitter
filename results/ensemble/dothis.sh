d03cefe451e63c607ad847161eda16381ebadaf4
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-04-13 10:30:23 +0200

    new results for 50k pseudos

--- results/ensemble/dothis.sh
+++ results/ensemble/dothis.sh
@@ -1,2 +1,7 @@
-RunFitter.exe -M MIGRAD -d ./extractedSV0.root -c /home/psteinb/development/sandbox/fitter/functions/SimpleMaxLLH.env -D "data_muid" -T "z_muid_trueB,z_muid_trueC,z_muid_trueL"
-RunFitter.exe -M MINOS  -d ./extractedSV0.root -c /home/psteinb/development/sandbox/fitter/functions/SimpleMaxLLH.env -D "data_muid" -T "z_muid_trueB,z_muid_trueC,z_muid_trueL"
+set -x
+RunPseudoExperiments.exe -M MINOS -d ./extractedSV0.root -c $SRC/functions/SimpleMaxLLH.env -D "data_muid" -T "z_muid_trueB,z_muid_trueC,z_muid_trueL" -o $SRC/results/ensemble/results.10k.eps -i 10000 > $SRC/results/ensemble/test.10k.log 2>&1 &
+
+RunPseudoExperiments.exe -M MINOS -d ./extractedSV0.root -c $SRC/functions/SimpleMaxLLH.env -D "data_muid" -T "z_muid_trueB,z_muid_trueC,z_muid_trueL" -o $SRC/results/ensemble/results.50k.eps -i 50000 > $SRC/results/ensemble/test.50k.log 2>&1 &
+
+
+set +x
