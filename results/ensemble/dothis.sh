27fcd2a3d575f0bc880d6c95369f451530e04682
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-04-27 00:34:32 +0200

    updated MakeRec by Studies

--- results/ensemble/dothis.sh
+++ results/ensemble/dothis.sh
@@ -1,7 +1,6 @@
 set -x
 RunPseudoExperiments.exe -M Migrad -d ./extractedSV0.root -c $SRC/functions/SimpleMaxLLH.env -D "data_muid" -T "z_muid_trueB,z_muid_trueC,z_muid_trueL" -o $SRC/results/ensemble/results.10k.eps -i 10000 > $SRC/results/ensemble/test.10k.log 2>&1 &
 
-RunPseudoExperiments.exe -M Migrad -d ./extractedSV0.root -c $SRC/functions/SimpleMaxLLH.env -D "data_muid" -T "z_muid_trueB,z_muid_trueC,z_muid_trueL" -o $SRC/results/ensemble/results.50k.eps -i 50000 > $SRC/results/ensemble/test.50k.log 2>&1 &
 
 
 set +x
