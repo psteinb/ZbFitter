3e20d0dadc07bbc392a9871011b6fc03f1d753ce
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-06-15 23:10:08 +0200

    refactored printing into methods

--- run/RunPseudoExperiments.cc
+++ run/RunPseudoExperiments.cc
@@ -30,8 +30,233 @@
 
 #include "AtlasStyle.h"
 
+void printAll(const std::vector<std::vector<TH1*> >& _results,const std::vector<double>& _expected,const std::vector<double>& _expectedErrors, TStyle* _aStyle, const std::string& _file=""){
 
 
+  TCanvas myResults(_file.c_str(),"",3000,2000);
+  myResults.Clear();
+  myResults.Draw();
+  myResults.Divide(3,_results.size());
+  int padSize = 3*_results.size();
+  TLine anLine;
+  anLine.SetLineColor(kBlue);
+  anLine.SetLineWidth(2);
+  int currentPad=0;
+  int padStart=1;
+  std::ostringstream expValueString;
+  TPaveText expValue;
+  expValue.SetTextColor(kBlue);
+  expValue.SetBorderSize(0.1);
+  expValue.SetFillColor(kWhite);
+
+  TVirtualPad* currentPadPtr = 0;
+  double ArrowXNDC = 0.;
+  for (int i = 0; i < _results.size(); ++i,padStart+=3)
+  {
+    expValueString.str("");
+    expValueString <</* "expected:\t"<<*/ (_expected[i]) << " #pm " << (_expectedErrors[i]);
+    for (int pad = 0; pad < 3; ++pad)
+    {
+      currentPad= pad + padStart;
+      currentPadPtr = myResults.cd(currentPad);
+      expValue.Clear();
+      _results[i][pad]->SetMaximum(1.4*_results[i][pad]->GetMaximum());
+      _results[i][pad]->Draw("");
+      if(pad<1){
+        myResults.Update();
+        ArrowXNDC = (gPad->XtoPad(_expected[i]) - gPad->GetUxmin())/(gPad->GetUxmax() - gPad->GetUxmin());
+        if(ArrowXNDC<.5)
+          expValue.SetX1NDC(.5);
+        else
+          expValue.SetX1NDC(.17);
+
+        expValue.SetY1NDC(1-_aStyle->GetStatH()-.17);
+        expValue.SetX2NDC(expValue.GetX1NDC()+.33);
+        expValue.SetY2NDC(expValue.GetY1NDC()+.1);
+
+
+        expValue.AddText(expValueString.str().c_str());
+        expValue.DrawClone();
+        
+        anLine.DrawLine(gPad->XtoPad(_expected[i]),gPad->GetUymin(),
+                        gPad->XtoPad(_expected[i]),gPad->GetUymax()/*,0.03,"<|"*/);
+      }
+    }
+  }
+  myResults.Update();
+  myResults.Print(".eps");
+
+
+}
+
+void printMeans(const std::vector<std::vector<TH1*> >& _results, const std::vector<double>& _expected,const std::string& _file="", ConfPseudoExperiment* _conf=0){
+
+  TCanvas MeanCanvas(_file.c_str(),"",3000,1000);
+  MeanCanvas.Clear();
+  MeanCanvas.Draw();
+  int width = _results.size();
+  TString templateNames = _conf->p_tempTitle.c_str();
+  templateNames.ToLower();
+
+  if(templateNames.Contains("ttbar") || templateNames.Contains("top"))
+    width-=1;
+
+  if(templateNames.Contains("qcd"))
+    width-=1;
+  
+  MeanCanvas.Divide(width,1);
+  int pad=1;
+  double ArrowXNDC = 0.;
+  TLine anLine;
+  anLine.SetLineColor(kBlue);
+  anLine.SetLineWidth(2);
+  TString title = "";
+  for (int i = 0; i < _results.size(); ++i,pad++)
+  {
+    title = _results[i][0]->GetTitle();
+    title.ToLower();
+    if(templateNames.Contains("ttbar") || templateNames.Contains("top") || templateNames.Contains("qcd"))
+      continue;
+
+    MeanCanvas.cd(pad);
+
+    _results[i][0]->Draw();
+    MeanCanvas.Update();
+    ArrowXNDC = (gPad->XtoPad(_expected[i]) - gPad->GetUxmin())/(gPad->GetUxmax() - gPad->GetUxmin());
+    anLine.DrawLine(gPad->XtoPad(_expected[i]),gPad->GetUymin(),
+                    gPad->XtoPad(_expected[i]),gPad->GetUymax()/*,0.03,"<|"*/);
+    // }
+
+  }
+  MeanCanvas.Update();
+  MeanCanvas.Print(".eps");
+
+}
+
+void printPulls(const std::vector<std::vector<TH1*> >& _results, const std::string& _file="", ConfPseudoExperiment* _conf=0){
+
+  TCanvas PullCanvas(_file.c_str(),"",3000,1500);
+  PullCanvas.Clear();
+  PullCanvas.Draw();
+
+  int width = _results.size();
+  TString templateNames = _conf->p_tempTitle.c_str();
+  templateNames.ToLower();
+
+  if(templateNames.Contains("ttbar") || templateNames.Contains("top"))
+    width-=1;
+
+  if(templateNames.Contains("qcd"))
+    width-=1;
+
+  PullCanvas.Divide(width,1);
+  int pad=1;
+  TF1* gaus = new TF1("MyGaus","gaus(0)",_results[0][2]->GetXaxis()->GetXmin(),_results[0][2]->GetXaxis()->GetXmax());
+  gaus->SetParameter(0,_results[0][2]->GetEntries());
+  gaus->SetParameter(1,0.);
+  gaus->SetParameter(2,1.);
+  TPaveText fitValues;
+  fitValues.SetTextColor(kBlue);
+  fitValues.SetBorderSize(0.1);
+  fitValues.SetFillColor(kWhite);
+  std::ostringstream stream;
+  TString title = "";
+  for (int i = 0; i < _results.size(); ++i,pad++)
+  {
+    stream.str("");
+    fitValues.Clear();
+    title = _results[i][2]->GetTitle();
+    title.ToLower();
+    if(templateNames.Contains("ttbar") || templateNames.Contains("top") || templateNames.Contains("qcd"))
+      continue;
+    PullCanvas.cd(pad);
+
+    gPad->SetTopMargin(.2);
+    _results[i][2]->SetStats(false);
+    _results[i][2]->Draw("e0");
+    if(_results[i][2]->GetEntries()>0){
+    _results[i][2]->Fit(gaus,"VR+");
+    stream << "#mu :\t" << gaus->GetParameter(1) << " #pm " << gaus->GetParError(1);
+    fitValues.AddText(stream.str().c_str());
+    stream.str("");
+    stream << "#sigma :\t" << gaus->GetParameter(2) << " #pm " << gaus->GetParError(2);
+    fitValues.AddText(stream.str().c_str());
+    stream.str("");
+    stream << "chi2/NDF :\t" << gaus->GetChisquare() << " / " << gaus->GetNDF();
+    fitValues.AddText(stream.str().c_str());
+    fitValues.SetX1NDC(.15);
+    fitValues.SetX2NDC(.85);
+    fitValues.SetY1NDC(.82);
+    fitValues.SetY2NDC(.98);
+    fitValues.DrawClone();
+    }
+    PullCanvas.Update();
+  }
+  PullCanvas.Update();
+  PullCanvas.Print(".eps");
+
+}
+
+void printCorrelations(const std::vector<std::vector<TH2*> >& _results, const std::string& _file=""){
+
+  int ysize = 0;
+  int xsize = 0;
+  if(_results.size() && _results[0].size()){
+    xsize = _results.size();
+    ysize = _results[0].size();
+
+
+    TCanvas PseudoCorrelation(_file.c_str(),"",xsize*800,ysize*600);
+
+    PseudoCorrelation.Clear();
+    PseudoCorrelation.Draw();
+    PseudoCorrelation.Divide(xsize,ysize);
+    
+    int padId = 0;
+    std::ostringstream correlationText;
+    for (int col = 0; col < _results.size(); ++col)
+    {
+      for (int row = 0; row < _results[0].size(); ++row)
+      {
+        padId = (_results.size()*row)+1+col;
+        PseudoCorrelation.cd(padId);
+        _results[col][row]->SetStats(false);
+        _results[col][row]->Draw("colz");
+
+        correlationText.str("");
+        correlationText<< "correlation: " << _results[col][row]->GetCorrelationFactor();
+        TPaveText aPave;
+        aPave.SetFillColor(kWhite);
+        aPave.SetBorderSize(0.1);
+        aPave.AddText(correlationText.str().c_str());
+        aPave.SetX1NDC(.1);
+        aPave.SetX2NDC(.9);
+        aPave.SetY1NDC(.85);
+        aPave.SetY2NDC(1.);
+        
+        aPave.DrawClone();
+        PseudoCorrelation.Update();
+      }
+    }
+    
+    PseudoCorrelation.Print(".eps");
+
+  }
+
+
+}
+
+void printMaxLLH(TH1* _maxLLH, const std::string& _file=""){
+
+ TCanvas PseudoMaxLLH(_file.c_str(),"",800,600);
+  PseudoMaxLLH.Clear();
+  PseudoMaxLLH.Draw();
+  PseudoMaxLLH.cd(0);
+  _maxLLH->Draw();
+  PseudoMaxLLH.Update();
+  PseudoMaxLLH.Print(".eps");
+
+}
 
 
 void createExpectedValuesFromTemplates(const std::vector<TH1*>& _templates,
@@ -108,7 +333,7 @@ int main(int argc, char* argv[])
     if(conf.p_msgLevel)
       conf.printConf();
 
-   // ----- INPUT ----- 
+  // ----- INPUT ----- 
   FitterInputs::NormedTH1<FitterInputs::Norm2Unity>* input = new FitterInputs::NormedTH1<FitterInputs::Norm2Unity>();
   input->loadData(conf.p_datadir.c_str(),conf.p_dataTitle.c_str(),conf.p_rebin);
   input->loadTemplates(conf.p_datadir.c_str(),conf.p_tempTitle.c_str(),conf.p_rebin);
@@ -117,7 +342,7 @@ int main(int argc, char* argv[])
   input->getTemplatesDeepCopy(m_templates);
   TH1* m_data =  input->getDataDeepCopy();
 
-   // ----- EXPECTED VALUES ----- 
+  // ----- EXPECTED VALUES ----- 
   MCValues aProtoMaker(conf.p_datadir.c_str(),conf.p_protoTitle,conf.p_rebin);
   std::vector<TH1*> m_protoFunctions(aProtoMaker.getProtoFunctions()->begin(),
                                      aProtoMaker.getProtoFunctions()->end());
@@ -126,21 +351,17 @@ int main(int argc, char* argv[])
   std::vector<double> expected          ;
   std::vector<double> expectedErrors    ;
   if(conf.p_protoTitle.empty())
-  createExpectedValuesFromTemplates(m_templates,
-                                    expected,          
-                                    expectedErrors,
-                                    m_data->Integral()*TMath::Abs(conf.p_dataScale));
+    createExpectedValuesFromTemplates(m_templates,
+                                      expected,          
+                                      expectedErrors,
+                                      m_data->Integral()*TMath::Abs(conf.p_dataScale));
   else
     createExpectedValuesFromTemplates(m_protoFunctions,
                                       expected,          
                                       expectedErrors,
                                       m_data->Integral()*TMath::Abs(conf.p_dataScale));
-  // createExpectedValuesFromTemplates(m_templates,
-  //                                   expected,          
-  //                                   expectedErrors,
-  //                                   m_data->Integral()*TMath::Abs(conf.p_dataScale));
 
-   // ----- PSEUDO EXPERIMENTS ----- 
+  // ----- PSEUDO EXPERIMENTS ----- 
   double scaleExpectation = TMath::Abs(conf.p_dataScale);
   PseudoStudy<FitterInputs::NormedTH1<FitterInputs::Norm2Unity>, functions::BinnedEML>  
     aPseudoStudy(m_templates,
@@ -165,201 +386,42 @@ int main(int argc, char* argv[])
   
   
 
-   // ----- COLLECT RESULTS ----- 
+  // ----- COLLECT RESULTS ----- 
   std::vector<std::vector<TH1*> > m_results(m_templates.size());
   for (int i = 0; i < m_templates.size(); ++i)
   {
     aPseudoStudy.getResultsOfParameter(i,m_results[i]);
   }
-  
 
+  // ----- DRAW RESULTS IN ONE CANVAS -----   
+  printAll(m_results,expected,expectedErrors,aStyle,conf.p_outputfile);
 
-  // ----- DRAW RESULTS IN ONE PAD ----- 
-  TCanvas myResults(conf.p_outputfile.c_str(),"",3000,2000);
-  myResults.Clear();
-  myResults.Draw();
-  myResults.Divide(3,m_templates.size());
-  int padSize = 3*m_templates.size();
-  TLine anLine;
-  anLine.SetLineColor(kBlue);
-  anLine.SetLineWidth(2);
-  int currentPad=0;
-  int padStart=1;
-  std::ostringstream expValueString;
-  TPaveText expValue;
-  expValue.SetTextColor(kBlue);
-  expValue.SetBorderSize(0.1);
-  expValue.SetFillColor(kWhite);
-
-  TVirtualPad* currentPadPtr = 0;
-  double ArrowXNDC = 0.;
-  for (int i = 0; i < m_results.size(); ++i,padStart+=3)
-  {
-    expValueString.str("");
-    expValueString <</* "expected:\t"<<*/ (expected[i]) << " #pm " << (expectedErrors[i]);
-    for (int pad = 0; pad < 3; ++pad)
-    {
-      currentPad= pad + padStart;
-      currentPadPtr = myResults.cd(currentPad);
-      expValue.Clear();
-      m_results[i][pad]->SetMaximum(1.4*m_results[i][pad]->GetMaximum());
-      m_results[i][pad]->Draw("");
-      if(pad<1){
-        myResults.Update();
-        ArrowXNDC = (gPad->XtoPad(expected[i]) - gPad->GetUxmin())/(gPad->GetUxmax() - gPad->GetUxmin());
-        if(ArrowXNDC<.5)
-          expValue.SetX1NDC(.5);
-        else
-          expValue.SetX1NDC(.17);
-
-        expValue.SetY1NDC(1-aStyle->GetStatH()-.17);
-        expValue.SetX2NDC(expValue.GetX1NDC()+.33);
-        expValue.SetY2NDC(expValue.GetY1NDC()+.1);
-
-
-        expValue.AddText(expValueString.str().c_str());
-        expValue.DrawClone();
-        
-        anLine.DrawLine(gPad->XtoPad(expected[i]),gPad->GetUymin(),
-                        gPad->XtoPad(expected[i]),gPad->GetUymax()/*,0.03,"<|"*/);
-      }
-    }
-  }
-  myResults.Update();
-  myResults.Print(".eps");
-
-    // ----- DRAW MEAN RESULTS IN ONE PAD ----- 
+  // ----- DRAW ONLY MEAN RESULTS ----- 
   std::string name = conf.p_outputfile;
   name += "_means";
-  TCanvas MeanCanvas(name.c_str(),"",3000,1000);
-  MeanCanvas.Clear();
-  MeanCanvas.Draw();
-  MeanCanvas.Divide(m_templates.size(),1);
-  int pad=1;
+  printMeans(m_results,expected,name);
 
-
-  for (int i = 0; i < m_results.size(); ++i,pad++)
-  {
-    MeanCanvas.cd(pad);
-
-    m_results[i][0]->Draw();
-    MeanCanvas.Update();
-    ArrowXNDC = (gPad->XtoPad(expected[i]) - gPad->GetUxmin())/(gPad->GetUxmax() - gPad->GetUxmin());
-    anLine.DrawLine(gPad->XtoPad(expected[i]),gPad->GetUymin(),
-                    gPad->XtoPad(expected[i]),gPad->GetUymax()/*,0.03,"<|"*/);
-    // }
-
-  }
-  MeanCanvas.Update();
-  MeanCanvas.Print(".eps");
-
-    // ----- DRAW PULL RESULTS IN ONE PAD ----- 
+  // ----- DRAW ONLY PULL RESULTS  ----- 
   name = conf.p_outputfile;
   name += "_pulls";
-  TCanvas PullCanvas(name.c_str(),"",3000,1500);
-  PullCanvas.Clear();
-  PullCanvas.Draw();
-  PullCanvas.Divide(m_templates.size(),1);
-  pad=1;
-  TF1* gaus = new TF1("MyGaus","gaus(0)",m_results[0][2]->GetXaxis()->GetXmin(),m_results[0][2]->GetXaxis()->GetXmax());
-  gaus->SetParameter(0,m_results[0][2]->GetEntries());
-  gaus->SetParameter(1,0.);
-  gaus->SetParameter(2,1.);
-  TPaveText fitValues;
-  fitValues.SetTextColor(kBlue);
-  fitValues.SetBorderSize(0.1);
-  fitValues.SetFillColor(kWhite);
-  std::ostringstream stream;
-  for (int i = 0; i < m_results.size(); ++i,pad++)
-  {
-    stream.str("");
-    fitValues.Clear();
-    PullCanvas.cd(pad);
-
-    gPad->SetTopMargin(.2);
-    m_results[i][2]->SetStats(false);
-    m_results[i][2]->Draw("e0");
-    if(m_results[i][2]->GetEntries()>0){
-    m_results[i][2]->Fit(gaus,"VR+");
-    stream << "#mu :\t" << gaus->GetParameter(1) << " #pm " << gaus->GetParError(1);
-    fitValues.AddText(stream.str().c_str());
-    stream.str("");
-    stream << "#sigma :\t" << gaus->GetParameter(2) << " #pm " << gaus->GetParError(2);
-    fitValues.AddText(stream.str().c_str());
-    stream.str("");
-    stream << "chi2/NDF :\t" << gaus->GetChisquare() << " / " << gaus->GetNDF();
-    fitValues.AddText(stream.str().c_str());
-    fitValues.SetX1NDC(.15);
-    fitValues.SetX2NDC(.85);
-    fitValues.SetY1NDC(.82);
-    fitValues.SetY2NDC(.98);
-    fitValues.DrawClone();
-    }
-    PullCanvas.Update();
-  }
-  PullCanvas.Update();
-  PullCanvas.Print(".eps");
+  printPulls(m_results,name);
 
 
+  // ----- DRAW maxLLH RESULTS  ----- 
   name = conf.p_outputfile;
   name += "_maxLLH";
 
-  TCanvas PseudoMaxLLH(name.c_str(),"",800,600);
-  PseudoMaxLLH.Clear();
-  PseudoMaxLLH.Draw();
-  PseudoMaxLLH.cd(0);
-  // aPseudoStudy.getMaxLLHDistribution()->Print("all");
-  aPseudoStudy.getMaxLLHDistribution()->Draw();
-  PseudoMaxLLH.Update();
-  PseudoMaxLLH.Print(".eps");
+  printMaxLLH(aPseudoStudy.getMaxLLHDistribution(),name);
 
+  // ----- RETRIEVE Correlations RESULTS  ----- 
   std::vector<std::vector<TH2*> > m_correlations(m_templates.size());
   aPseudoStudy.getCorrelationResults(m_correlations);
-  int ysize = 0;
-  int xsize = 0;
-  if(m_correlations.size() && m_correlations[0].size()){
-    xsize = m_correlations.size();
-    ysize = m_correlations[0].size();
+  
+  // ----- DRAW Correlations RESULTS  ----- 
+  name = conf.p_outputfile;
+  name += "_correlations";
+  printCorrelations(m_correlations,name);
 
-    name = conf.p_outputfile;
-    name += "_correlations";
-
-    TCanvas PseudoCorrelation(name.c_str(),"",xsize*800,ysize*600);
-
-    PseudoCorrelation.Clear();
-    PseudoCorrelation.Draw();
-    PseudoCorrelation.Divide(xsize,ysize);
-    
-    int padId = 0;
-    std::ostringstream correlationText;
-    for (int col = 0; col < m_correlations.size(); ++col)
-    {
-      for (int row = 0; row < m_correlations[0].size(); ++row)
-      {
-        padId = (m_correlations.size()*row)+1+col;
-        PseudoCorrelation.cd(padId);
-        m_correlations[col][row]->SetStats(false);
-        m_correlations[col][row]->Draw("colz");
-
-        correlationText.str("");
-        correlationText<< "correlation: " << m_correlations[col][row]->GetCorrelationFactor();
-        TPaveText aPave;
-        aPave.SetFillColor(kWhite);
-        aPave.SetBorderSize(0.1);
-        aPave.AddText(correlationText.str().c_str());
-        aPave.SetX1NDC(.1);
-        aPave.SetX2NDC(.9);
-        aPave.SetY1NDC(.85);
-        aPave.SetY2NDC(1.);
-        
-        aPave.DrawClone();
-        PseudoCorrelation.Update();
-      }
-    }
-    
-    PseudoCorrelation.Print(".eps");
-
-  }
 
   aPseudoStudy.printResults();
   return 0; 
