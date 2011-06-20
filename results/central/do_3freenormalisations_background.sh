a5c4e20a928ac20583479fa94a396b682eb75819
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-06-20 15:36:01 +0200

    added script to run fit on all backgrounds

---
+++ results/central/do_3freenormalisations_background.sh
@@ -0,0 +1,6 @@
+#plain fit
+RunFitter.exe -M Migrad -d $SRC/Sherpa_added.root  -c $SRC/results/environments/SherpaAdded/BinnedEML.4params.Sherpa_added_top_QCD.env -D "Data" -T "Zjets_b,Zjets_c,Zjets_l,TTbar,QCD" -o Sherpa_3Normalisations_added_top_QCD -r 5 -m 6  
+
+#with goodness-of-fit (need to create argument to -p beforehand)
+#RunFitter.exe -M Migrad -d $SRC/Sherpa_added.root  -c $SRC/results/environments/SherpaAdded/BinnedEML.4params.Sherpa_added_top.env -D "Data" -T "Zjets_b,Zjets_c,Zjets_l,TTbar" -o Sherpa_3Normalisations_added_top -r 5 -m 6 -p $SRC/results/central/Sherpa_3Normalisations_added_15k_top_MeansSigmasPullsMaxLLH.root
+
