3f1d15f26de94fbfe0446326eb7521aa1a39e8e7
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-08-31 16:47:36 +0200

    found a bug in bundle ini files

--- bundles/sherpa/inclusive/createInputRootFiles.sh
+++ bundles/sherpa/inclusive/createInputRootFiles.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_allJets.cfg -o SherpaSV0.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetProb_70_allJets.cfg -o SherpaJetProb_70.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetProb_80_allJets.cfg -o SherpaJetProb_80.root
 python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_60_allJets.cfg -o SherpaJetCombNN_60.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_70_allJets.cfg -o SherpaJetCombNN_70.root
