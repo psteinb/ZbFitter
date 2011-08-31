df22a0ae8bfbaebf2f47bbb033dfed02c99ca0ea
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-08-31 17:07:43 +0200

    updated all config files

--- bundles/sherpa/NJetsEq1/createInputRootFiles.sh
+++ bundles/sherpa/NJetsEq1/createInputRootFiles.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsEq1.cfg -o SherpaSV0_NJetsEq1.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetProb_70_NJetsEq1.cfg -o SherpaJetProb_70_NJetsEq1.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetProb_80_NJetsEq1.cfg -o SherpaJetProb_80_NJetsEq1.root
 python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_60_NJetsEq1.cfg -o SherpaJetCombNN_60_NJetsEq1.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_70_NJetsEq1.cfg -o SherpaJetCombNN_70_NJetsEq1.root
