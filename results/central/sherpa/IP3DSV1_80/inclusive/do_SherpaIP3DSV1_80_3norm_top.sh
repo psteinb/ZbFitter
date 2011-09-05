b5f57ca8cc7d4dd371f18a0e8453e4b2e3485902
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-09-05 22:11:44 +0200

    added IP3DSV1 inclusive steering files for straigt fit

---
+++ results/central/sherpa/IP3DSV1_80/inclusive/do_SherpaIP3DSV1_80_3norm_top.sh
@@ -0,0 +1,8 @@
+RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_80_SV12Mass.root  -c SherpaIP3DSV1_80_3norm_top.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_80_3norm_top_inclusive -r 5 > $SRC/results/central/sherpa/IP3DSV1_80/inclusive/SherpaIP3DSV1_80_3norm_top.log 2>&1 &
+
+RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_80_SV12Mass.root  -c SherpaIP3DSV1_80_3norm_top.env -D "PseudoData" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_80_3norm_top_inclusive_PseudoData -r 5 > $SRC/results/central/sherpa/IP3DSV1_80/inclusive/SherpaIP3DSV1_80_3norm_top_PseudoData.log 2>&1 &
+
+RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_80_SV12Mass.root  -c SherpaIP3DSV1_80_2norm_fixedLight_fixedTop.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_inclusive -r 5 > $SRC/results/central/sherpa/IP3DSV1_80/inclusive/SherpaIP3DSV1_80_2norm_fixedLight_fixedTop.log 2>&1 &
+
+RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_80_SV12Mass.root  -c SherpaIP3DSV1_80_2norm_fixedLight_fixedTop.env -D "PseudoData" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_inclusive_PseudoData -r 5 > $SRC/results/central/sherpa/IP3DSV1_80/inclusive/SherpaIP3DSV1_80_2norm_fixedLight_fixedTop_PseudoData.log 2>&1 &
+
