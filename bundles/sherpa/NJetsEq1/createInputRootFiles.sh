a22f7d1c89bbf10a14e0b14cb4df62ca047e059e
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-09-06 10:13:08 +0200

    created new config files to create IP3DSV1 bundles

--- bundles/sherpa/NJetsEq1/createInputRootFiles.sh
+++ bundles/sherpa/NJetsEq1/createInputRootFiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsEq1.cfg -o SherpaSV0_NJetsEq1.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_60_NJetsEq1.cfg -o SherpaJetCombNN_60_NJetsEq1.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_70_NJetsEq1.cfg -o SherpaJetCombNN_70_NJetsEq1.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_NJetsEq1.cfg -o SherpaIP3DSV1_60_NJetsEq1.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsEq1.cfg -o SherpaIP3DSV1_70_NJetsEq1.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsEq1.cfg -o SherpaIP3DSV1_80_NJetsEq1.root
