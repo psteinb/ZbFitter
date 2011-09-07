15c39558598c536251c2e0b6da4c2bee45224fc0
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-09-07 14:21:50 +0200

    added fits on smoothed input histos to shell scripts

--- results/central/sherpa/IP3DSV1_70/NJetsGe2/do_SherpaIP3DSV1_70_3norm_top.sh
+++ results/central/sherpa/IP3DSV1_70/NJetsGe2/do_SherpaIP3DSV1_70_3norm_top.sh
@@ -10,6 +10,8 @@ RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe
 
 RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_maxSV1mass_lowc_HIGHb.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_NJetsGe2_maxSV1mass_lowc_HIGHb -r 5   > $SRC/results/central/sherpa/IP3DSV1_70/NJetsGe2/SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_maxSV1mass_lowc_HIGHb.log 2>&1 &
 
+RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_maxSV1mass.env -D "Data" -T "smoothedTrueB,smoothedTrueC,smoothedTrueL,smoothedTop" -o Sherpa_2norm_fixedLight_fixedTop_NJetsGe2_maxSV1mass_smoothed -r 5   > $SRC/results/central/sherpa/IP3DSV1_70/NJetsGe2/SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_maxSV1mass_lowc_HIGHb_smoothed.log 2>&1 &
+
 
 #RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_70_3norm_top_sumSV1mass.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_70_3norm_top_NJetsGe2_sumSV1mass -r 5   > $SRC/results/central/sherpa/IP3DSV1_70/NJetsGe2/SherpaIP3DSV1_70_3norm_top_sumSV1mass.log 2>&1 &
 
@@ -18,3 +20,5 @@ RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe
 RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass_HIGHc_lowb.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_NJetsGe2_sumSV1mass_HIGHc_lowb -r 5   > $SRC/results/central/sherpa/IP3DSV1_70/NJetsGe2/SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass_HIGHc_lowb.log 2>&1 &
 
 RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass_lowc_HIGHb.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_NJetsGe2_sumSV1mass_lowc_HIGHb -r 5   > $SRC/results/central/sherpa/IP3DSV1_70/NJetsGe2/SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass_lowc_HIGHb.log 2>&1 &
+
+RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass.env -D "Data" -T "smoothedTrueB,smoothedTrueC,smoothedTrueL,smoothedTop" -o Sherpa_2norm_fixedLight_fixedTop_NJetsGe2_sumSV1mass_smoothed -r 5   > $SRC/results/central/sherpa/IP3DSV1_70/NJetsGe2/SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass_smoothed.log 2>&1 &
