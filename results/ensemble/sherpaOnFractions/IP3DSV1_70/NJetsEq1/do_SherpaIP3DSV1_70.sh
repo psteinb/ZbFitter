3ed9229efc687c050c957090e52ee5a62df28657
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-09-08 11:04:26 +0200

    added toyMC tests for inclusive sample

--- results/ensemble/sherpaOnFractions/IP3DSV1_70/NJetsEq1/do_SherpaIP3DSV1_70.sh
+++ results/ensemble/sherpaOnFractions/IP3DSV1_70/NJetsEq1/do_SherpaIP3DSV1_70.sh
@@ -1,6 +1,6 @@
 
 RunPseudoExperimentsOnFractions.exe -M Migrad -d $SRC/bundles/sherpa/NJetsEq1/SherpaIP3DSV1_70_NJetsEq1.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_70_2norm_2fixed_NJetsEq1_fractions -r 5 -i 15000   > $SRC/results/ensemble/sherpaOnFractions/IP3DSV1_70/NJetsEq1/SherpaIP3DSV1_70_2norm_2fixed.log 2>&1 &
 
-RunPseudoExperimentsOnFractions.exe -M Migrad -d $SRC/bundles/sherpa/NJetsEq1/SherpaIP3DSV1_70_NJetsEq1.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop.env -D "PseudoData" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_70_2norm_2fixed_NJetsEq1_fractions_PseudoData -r 5 -i 15000   > $SRC/results/ensemble/sherpaOnFractions/IP3DSV1_70/NJetsEq1/SherpaIP3DSV1_70_2norm_2fixed_PseudoData.log 2>&1 &
+
 
 
---
+++ results/ensemble/sherpaOnFractions/IP3DSV1_70/inclusive/do_SherpaIP3DSV1_70.sh
@@ -0,0 +1,10 @@
+
+RunPseudoExperimentsOnFractions.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_70_sumSV12mass.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_inclusive_sumSV12mass -r 5 -i 15000 > $SRC/results/ensemble/sherpaOnFractions/IP3DSV1_70/inclusive/SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV12mass.log 2>&1 &
+
+
+
+
+RunPseudoExperimentsOnFractions.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_70_maxSV12mass.root  -c SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_maxSV1mass.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_inclusive_maxSV12mass -r 5 -i 15000 > $SRC/results/ensemble/sherpaOnFractions/IP3DSV1_70/inclusive/SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_maxSV12mass.log 2>&1 &
+
+
+
