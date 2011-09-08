2f046e0c5d1491b27fa6ccd4d621f699aa683ed4
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-09-08 10:36:32 +0200

    added config scripts for inclusive plots

--- bundles/sherpa/inclusive/createInputRootFiles.sh
+++ bundles/sherpa/inclusive/createInputRootFiles.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_allJets.cfg -o SherpaSV0.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_allJets.cfg -o SherpaIP3DSV1_60_SV12Mass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_allJets.cfg -o SherpaIP3DSV1_70_SV12Mass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_allJets.cfg -o SherpaIP3DSV1_80_SV12Mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_allJets_sumSV0mass.cfg -o SherpaSV0_sumSV0mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_allJets_sumSV12mass.cfg -o SherpaIP3DSV1_60_sumSV12Mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_allJets_sumSV12mass.cfg -o SherpaIP3DSV1_70_sumSV12Mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_allJets_sumSV12mass.cfg -o SherpaIP3DSV1_80_sumSV12Mass.root
+
+python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_allJets_maxSV0mass.cfg -o SherpaSV0_maxSV0mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_allJets_maxSV12mass.cfg -o SherpaIP3DSV1_60_maxSV12Mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_allJets_maxSV12mass.cfg -o SherpaIP3DSV1_70_maxSV12Mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_allJets_maxSV12mass.cfg -o SherpaIP3DSV1_80_maxSV12Mass.root
