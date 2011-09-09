5eaceb9d36b87b78299f7cd15c54f0999ce25178
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-09-09 16:16:08 +0200

    added IP3DSV1 80 to ensemble tests

---
+++ results/ensemble/sherpa/IP3DSV1_80/NJetsEq1/do_SherpaIP3DSV1_80.sh
@@ -0,0 +1,5 @@
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/NJetsEq1/SherpaIP3DSV1_80_NJetsEq1.root  -c SherpaIP3DSV1_80_3norm_top.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_80_3norm_top_NJetsEq1 -r 5 -i 15000   > $SRC/results/ensemble/sherpa/IP3DSV1_80/NJetsEq1/SherpaIP3DSV1_80_3norm_top.log 2>&1 &
+
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/NJetsEq1/SherpaIP3DSV1_80_NJetsEq1.root  -c SherpaIP3DSV1_80_2norm_fixedLight_fixedTop.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_NJetsEq1 -r 5 -i 15000   > $SRC/results/ensemble/sherpa/IP3DSV1_80/NJetsEq1/SherpaIP3DSV1_80_2norm_fixedLight_fixedTop.log 2>&1 &
+
+
---
+++ results/ensemble/sherpa/IP3DSV1_80/NJetsGe2/do_SherpaIP3DSV1_80_3norm_top.sh
@@ -0,0 +1,10 @@
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_80_NJetsGe2_maxSV12mass.root  -c SherpaIP3DSV1_80_3norm_top_sumSV1mass.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_80_3norm_top_maxSV12mass -r 5 -i 15000   > $SRC/results/ensemble/sherpa/IP3DSV1_80/NJetsGe2/SherpaIP3DSV1_80_3norm_top_sum_maxSV12mass.lg 2>&1 &
+
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_80_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_80_3norm_top_sumSV1mass.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o SherpaIP3DSV1_80_3norm_top_sumSV12mass -r 5 -i 15000   > $SRC/results/ensemble/sherpa/IP3DSV1_80/NJetsGe2/SherpaIP3DSV1_80_3norm_top_sumSV12mass.lg 2>&1 &
+
+
+
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_80_NJetsGe2_maxSV12mass.root  -c SherpaIP3DSV1_80_2norm_fixedLight_fixedTop_maxSV1mass.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_NJetsGe2_maxSV1mass -r 5 -i 15000   > $SRC/results/ensemble/sherpa/IP3DSV1_80/NJetsGe2/SherpaIP3DSV1_80_2norm_fixedLight_fixedTop_maxSV1mass.log 2>&1 &
+
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/NJetsGe2/SherpaIP3DSV1_80_NJetsGe2_sumSV12mass.root  -c SherpaIP3DSV1_80_2norm_fixedLight_fixedTop_sumSV1mass.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop_NJetsGe2_sumSV1mass -r 5 -i 15000   > $SRC/results/ensemble/sherpa/IP3DSV1_80/NJetsGe2/SherpaIP3DSV1_80_2norm_fixedLight_fixedTop_sumSV1mass.log 2>&1 &
+
