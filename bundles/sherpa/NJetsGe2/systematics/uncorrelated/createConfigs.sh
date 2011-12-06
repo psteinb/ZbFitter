6b4525dbf8d6201658d38c2ae35464b9be6e477a
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-06 16:44:36 +0100

    finished systematics bundles for sherpa

--- bundles/sherpa/NJetsGe2/systematics/uncorrelated/createConfigs.sh
+++ bundles/sherpa/NJetsGe2/systematics/uncorrelated/createConfigs.sh
@@ -1,10 +1,10 @@
 ITEMS="HeavyFlavorBTagEffDown HeavyFlavorBTagEffUp JER JESDOWN JESUP LightBTagEffDown LightBTagEffUp MuonEff MuonPt AllFlavorsBTagEffUp AllFlavorsBTagEffDown"
 
 for item in $ITEMS;do
-    NEWFILE80=bundleIP3DSV1_80_NJetsEq1_SV12mass_${item}.cfg 
-    NEWFILE70=bundleIP3DSV1_70_NJetsEq1_SV12mass_${item}.cfg 
-    cp bundleIP3DSV1_80_NJetsEq1_SV12mass_template.cfg ${NEWFILE80}
-    cp bundleIP3DSV1_70_NJetsEq1_SV12mass_template.cfg ${NEWFILE70}
+    NEWFILE80=bundleIP3DSV1_80_NJetsGe2_sumSV12mass_${item}.cfg 
+    NEWFILE70=bundleIP3DSV1_70_NJetsGe2_sumSV12mass_${item}.cfg 
+    cp bundleIP3DSV1_80_NJetsGe2_sumSV12mass_template.cfg ${NEWFILE80}
+    cp bundleIP3DSV1_70_NJetsGe2_sumSV12mass_template.cfg ${NEWFILE70}
     sed -i -e "s/JER/${item}/g" $NEWFILE80 $NEWFILE70
 done
     
