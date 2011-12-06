b6faab9d76cf1124186efef529371639b4305f4a
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-06 10:42:47 +0100

    finished merging script

--- python/mergeResults.py
+++ python/mergeResults.py
@@ -5,7 +5,7 @@ if basePath:
     sys.path.append(basePath+"/python")
 else:
     print "WARNING, PACKAGE NOT SETUP!"
-sys.path.append("/home/%s/bin/atlasstyle-trunk" % (os.getenv("USER")))
+sys.path.append("%s/bin/atlasstyle-trunk" % (os.getenv("HOME")))
 
 if not 'gROOT' in dir():
     import ROOT
@@ -65,7 +65,7 @@ class BundleHistos:
             filename = self.configFile.split("/")[-1]
 
             self.objectDB.setConfigFile(self.configFile)
-            self.objectDB.openFiles(False)
+            #self.objectDB.openFiles(False)
             self.initDictFromDatabase()
         else:
             if self.parsedOpts['fileHistoList']:
@@ -99,84 +99,104 @@ class BundleHistos:
         """
         self.loaded = {}
         self.loadedKeys = {}
-        for key,value in self.objectDB.loadedDict.iteritems():
-            for item in value:
-                self.loaded[item] = key
+        #for key,value in self.objectDB.loadedDict.iteritems():
+        #    for item in value:
+        #        self.loaded[item] = key
 
         for sec in self.objectDB.cfgParser.sections():
             self.loadedKeys[sec] = {}
             for k,v in self.objectDB.cfgParser.items(sec):
                 if v.lower().count(".root"):
-                    continue
+                    fileName = self.replaceEnvironment(v)
+                    print "opening ",fileName
+                    self.openedFiles.append(ROOT.TFile.Open(fileName))
+                    self.loaded[sec] = self.openedFiles[-1]
                 else:
                     self.loadedKeys[sec][k] = v
 
         return 0
 
+    def replaceEnvironment(self,_text):
+        value = _text
+        if _text.count("__")>1:
+            dirItems = _text.split("/")
+            envItem = [item for item in dirItems if item.count("__")>1][0]
+            envName = envItem[envItem.find("__")+2:envItem.rfind("__")]
+            value = _text.replace(envItem,os.getenv(envName))
+            
+        return value
+
     def collectData(self):
 
         self.nBins = len(self.loadedKeys.keys())
-        
-
+        print "merging into %i bins" % self.nBins
         centralValueObjs = [ item.Get("centralValues") for item in self.openedFiles if item.Get("centralValues").__nonzero__() ]
         if len(centralValueObjs):
             self.nFits = centralValueObjs[0].GetNbinsX()
             for item in range(1,self.nFits+1):
                 self.fitNames.append(str(centralValueObjs[0].GetXaxis().GetBinLabel(item)))
-            
+        print "found %i fits\n%s" % (self.nFits,self.fitNames)
                 
-        for item,content in self.loadedKeys:
-            if not self.obsName and content.has_key("ObservableName"):
-                self.obsName = content["ObservableName"]
+        for item,content in self.loadedKeys.iteritems():
+            if not self.obsName and content.has_key("observablename"):
+                self.obsName = content["observablename"]
+                print "observable to process ", self.obsName
 
-            if content.has_key("binID"):
-                self.BinBorders.insert(float(content["binID"]))
-            elif content.has_key("binValue"):
-                self.BinBorders.insert(float(content["binValue"].split(",")[0]))
-                if len(content["binValue"].split(","))>1:
-                    self.BinBorders.insert(float(content["binValue"].split(",")[-1]))
+            if content.has_key("binid"):
+                self.BinBorders.add(float(content["binid"]))
+            elif content.has_key("binvalue"):
+                binValue = content["binvalue"]
+                self.BinBorders.add(float(binValue.split(",")[0]))
+                if len(binValue.split(","))>1:
+                    self.BinBorders.add(float(content["binvalue"].split(",")[-1]))
             else:
                 print ">> %s does not have binID nor binValue, skipping \n%s" % (item,content)
-
+                
         self.UpperBinLimit = max(self.BinBorders)
         self.LowerBinLimit = min(self.BinBorders)
-        
+        print "[%.2f,%.2f] %s" % (self.LowerBinLimit,self.UpperBinLimit,self.BinBorders)
         return
 
     def createResultHistos(self):
         self.outFile = ROOT.TFile(self.outputname,"RECREATE")
+        self.outFile.WriteFree()
 
         for idx in range(self.nFits):
-            self.Histos2Write.append(ROOT.TH1D(self.fitNames[idx]+"_symm",";%s;N_{%s}" % (self.obsName,self.fitNames[idx]),self.nBins,self.LowerBinLimit,self.UpperBinLimit))
+            currentName = self.fitNames[idx].replace("{","").replace("}","")
+            self.Histos2Write.append(ROOT.TH1D(currentName+"_symm",";%s;%s" % (self.obsName,currentName),self.nBins,self.LowerBinLimit,self.UpperBinLimit))
             self.Graphs2Write.append(ROOT.TGraphAsymmErrors(self.nBins))
-            self.Graphs2Write[-1].GetYaxis().SetName("N_{%s}" % self.fitNames[idx])
+            self.Graphs2Write[-1].GetYaxis().SetName(self.fitNames[idx])
             self.Graphs2Write[-1].GetYaxis().SetName(self.obsName)
-            self.Graphs2Write[-1].SetName(self.fitNames[idx]+"_asymm")
+            self.Graphs2Write[-1].SetName(currentName+"_asymm")
             
         return
 
     def fillResultHistos(self):
 
-        for abin in range(1,self.nBins+1):
+        for abin in range(self.nBins):
+            currentFile = self.openedFiles[abin]
+            print ">> running on file",currentFile.GetName()
             for fit in range(self.nFits):
 
-                central = self.openedFiles[abin].Get("centralValues").GetBinContent(abin)
-                symmUncert = self.openedFiles[abin].Get("SymmErrors").GetBinContent(abin)
-                UncertUp = self.openedFiles[abin].Get("AsymmErrorsUp").GetBinContent(abin)
-                UncertDown = self.openedFiles[abin].Get("AsymmErrorsDown").GetBinContent(abin)
-                binUncert = self.Histos2Write[fit].GetXaxis().GetBinWidth(abin)/2
-                binCenter = self.Histos2Write[fit].GetXaxis().GetBinCenter(abin)
+                central = currentFile.Get("centralValues").GetBinContent(fit+1)
+                symmUncert = currentFile.Get("SymmErrors").GetBinContent(fit+1)
+                UncertUp = currentFile.Get("AsymmErrorsUp").GetBinContent(fit+1)
+                UncertDown = currentFile.Get("AsymmErrorsDown").GetBinContent(fit+1)
+                binUncert = self.Histos2Write[fit].GetXaxis().GetBinWidth(abin+1)/2
+                binCenter = self.Histos2Write[fit].GetXaxis().GetBinCenter(abin+1)
 
-                self.Histos2Write[fit].SetBinContent(abin,central)
-                self.Histos2Write[fit].SetBinError(abin,symmUncert)
+                self.Histos2Write[fit].SetBinContent(abin+1,central)
+                self.Histos2Write[fit].SetBinError(abin+1,symmUncert)
                 self.Graphs2Write[fit].SetPoint(abin,binCenter,central)
                 self.Graphs2Write[fit].SetPointError(abin,binUncert,binUncert,UncertDown,UncertUp)
-                print "[f:%i,bin:%i] %s: added [x,y] = %.2f, %.2f; sigma(x) = %.2f, sigma(y) = %.2f, <up/down> = %.2f/%.2f" % (fit,abin,self.fitnames[fit], binCenter,central,binUncert,symmUncert,UncertUp,UncertDown)
+                print "[f:%i,bin:%i] %s: added [x,y] = %.2f, %.2f; sigma(x) = %.2f, sigma(y) = %.2f, <up/down> = %.2f/%.2f" % (fit,abin,self.fitNames[fit], binCenter,central,binUncert,symmUncert,UncertUp,UncertDown)
 
         return
     
     def writeAll(self):
         
+        for graph in self.Graphs2Write:
+            graph.Write()
         self.outFile.Write()
         self.outFile.Close()
 
@@ -192,23 +212,23 @@ class BundleHistos:
 
         try:
             self.collectData()
-        except Exception:
-            print "collectData failed due to ",Exception
+        except Exception as inst:
+            print "collectData failed due to ",inst
 
         try:
             self.createResultHistos()
-        except Exception:
-            print "createResultHistos failed due to ",Exception
+        except Exception as inst:
+            print "createResultHistos failed due to ",inst
             
         try:
             self.fillResultHistos()
-        except Exception:
-            print "fillResultHistos failed due to ",Exception
+        except Exception as inst:
+            print "fillResultHistos failed due to ",inst
 
         try:
             self.writeAll()
-        except Exception:
-            print "writeAll failed due to ",Exception
+        except Exception as inst:
+            print "writeAll failed due to ",inst
             
         
         # nfile = ROOT.TFile.Open(self.outputname,"RECREATE")
