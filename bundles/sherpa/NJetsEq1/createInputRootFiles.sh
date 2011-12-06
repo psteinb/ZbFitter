e8de44b773e77b5a786001589798d83a0693ca96
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-06 15:12:59 +0100

    started to update sherpa steering files

--- bundles/sherpa/NJetsEq1/createInputRootFiles.sh
+++ bundles/sherpa/NJetsEq1/createInputRootFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsEq1.cfg -o SherpaSV0_NJetsEq1.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_NJetsEq1.cfg -o SherpaIP3DSV1_60_NJetsEq1.root
+#python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsEq1.cfg -o SherpaSV0_NJetsEq1.root
+#python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_NJetsEq1.cfg -o SherpaIP3DSV1_60_NJetsEq1.root
 python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsEq1.cfg -o SherpaIP3DSV1_70_NJetsEq1.root
 python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsEq1.cfg -o SherpaIP3DSV1_80_NJetsEq1.root
--- bundles/sherpa/NJetsGe2/createInputRootFiles.sh
+++ bundles/sherpa/NJetsGe2/createInputRootFiles.sh
@@ -5,10 +5,9 @@ python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsGe2.cfg -o SherpaSV0_
 #python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsGe2.cfg -o SherpaIP3DSV1_70_NJetsGe2.root
 #python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2.cfg -o SherpaIP3DSV1_80_NJetsGe2.root
 #
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_NJetsGe2_maxSV12mass.cfg -o SherpaIP3DSV1_60_NJetsGe2_maxSV12mass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsGe2_maxSV12mass.cfg -o SherpaIP3DSV1_70_NJetsGe2_maxSV12mass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2_maxSV12mass.cfg -o SherpaIP3DSV1_80_NJetsGe2_maxSV12mass.root
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_60_NJetsGe2_sumSV12mass.cfg -o SherpaIP3DSV1_60_NJetsGe2_sumSV12mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsGe2_SV12mass.cfg -o SherpaIP3DSV1_70_NJetsGe2_SV12mass.root
+python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2_SV12mass.cfg -o SherpaIP3DSV1_80_NJetsGe2_SV12mass.root
+
 python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsGe2_sumSV12mass.cfg -o SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root
 python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2_sumSV12mass.cfg -o SherpaIP3DSV1_80_NJetsGe2_sumSV12mass.root
