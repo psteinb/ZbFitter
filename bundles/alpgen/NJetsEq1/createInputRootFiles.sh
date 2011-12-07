a4d93169c124acc30c37dc84e9e532c50769b85d
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-07 06:52:26 +0100

    finished alpgen configs for NJetsEq1 final state

--- bundles/alpgen/NJetsEq1/createInputRootFiles.sh
+++ bundles/alpgen/NJetsEq1/createInputRootFiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsEq1.cfg -o SherpaSV0_NJetsEq1.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_60_NJetsEq1.cfg -o SherpaJetCombNN_60_NJetsEq1.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_70_NJetsEq1.cfg -o SherpaJetCombNN_70_NJetsEq1.root
+for file in *cfg;
+do 
+    echo "processing file ${file}"
+    python $SRC/python/bundleHistos.py -f `pwd`/$file -o ${file/cfg/root}
+done
