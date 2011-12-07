75fc9183a957b236337c68221ffd7c28721b84ba
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-07 06:54:45 +0100

    finished alpgen configs for NJetsGe2 final state

--- bundles/alpgen/NJetsGe2/createInputRootFiles.sh
+++ bundles/alpgen/NJetsGe2/createInputRootFiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsGe2.cfg -o SherpaSV0_NJetsGe2.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_60_NJetsGe2.cfg -o SherpaJetCombNN_60_NJetsGe2.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_70_NJetsGe2.cfg -o SherpaJetCombNN_70_NJetsGe2.root
+for file in *cfg;
+do 
+    echo "processing file ${file}"
+    python $SRC/python/bundleHistos.py -f `pwd`/$file -o ${file/cfg/root}
+done
