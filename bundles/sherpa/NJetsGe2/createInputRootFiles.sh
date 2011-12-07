8bdcaac61c1a5623f8261324206b7cb61ecf0f1b
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-07 14:00:04 +0100

    bundles for NJetsGe2 updated again and cleaned up

--- bundles/sherpa/NJetsGe2/createInputRootFiles.sh
+++ bundles/sherpa/NJetsGe2/createInputRootFiles.sh
@@ -6,8 +6,8 @@ python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_NJetsGe2.cfg -o SherpaSV0_
 #python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2.cfg -o SherpaIP3DSV1_80_NJetsGe2.root
 #
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsGe2_SV12mass.cfg -o SherpaIP3DSV1_70_NJetsGe2_SV12mass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2_SV12mass.cfg -o SherpaIP3DSV1_80_NJetsGe2_SV12mass.root
+#python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsGe2_SV12mass.cfg -o SherpaIP3DSV1_70_NJetsGe2_SV12mass.root
+#python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2_SV12mass.cfg -o SherpaIP3DSV1_80_NJetsGe2_SV12mass.root
 
 python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_NJetsGe2_sumSV12mass.cfg -o SherpaIP3DSV1_70_NJetsGe2_sumSV12mass.root
 python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_NJetsGe2_sumSV12mass.cfg -o SherpaIP3DSV1_80_NJetsGe2_sumSV12mass.root
