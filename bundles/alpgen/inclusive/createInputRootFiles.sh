b567e3faf0c0219ae74c7e449157cbfea6aeb806
Peter Steinbach <psteinb@tragling.(none)>
2011-12-07 06:48:35 +0100

    finished alpgen stuff for inclusive final state

--- bundles/alpgen/inclusive/createInputRootFiles.sh
+++ bundles/alpgen/inclusive/createInputRootFiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_allJets.cfg -o SherpaSV0.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_60_allJets_jFitMass.cfg -o SherpaJetCombNN_60_jFitMass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleJetCombNN_70_allJets_jFitMass.cfg -o SherpaJetCombNN_70_jFitMass.root
+for file in *cfg;
+do 
+    echo "processing file ${file}"
+    python $SRC/python/bundleHistos.py -f `pwd`/$file -o ${file/cfg/root}
+done
