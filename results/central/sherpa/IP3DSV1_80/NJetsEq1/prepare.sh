b1772e3c8e0972cf522e33202f38cf077129ed1f
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-14 13:05:18 +0100

    updated fit with histos reaching down to negative values in m_sv

---
+++ results/central/sherpa/IP3DSV1_80/NJetsEq1/prepare.sh
@@ -0,0 +1,2 @@
+python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/NJetsEq1/SherpaIP3DSV1_80_NJetsEq1.root -3|egrep "^[A-Za-z]" > SherpaIP3DSV1_80_3norm_top.env
+python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/NJetsEq1/SherpaIP3DSV1_80_NJetsEq1.root -2|egrep "^[A-Za-z]" > SherpaIP3DSV1_80_2norm_fixedLight_fixedTop.env 
