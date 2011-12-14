513e2da7e1835d733ee222e3e79a31756d8ad9ee
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-14 15:02:13 +0100

    cleaned up central results

--- results/central/do_1o3fractions_top.sh
+++
@@ -1,6 +0,0 @@
-RunFitterOnFractions.exe -M Migrad -d $SRC/Sherpa_ee.root  -c $SRC/results/environments/Sherpa/BinnedEMLFraction.1o3.Sherpa_ee_top.env -D "Data" -T "Zjets_b,Zjets_l,TTbar,Zjets_c" -o Sherpa_3fractionfit_ee_top -r 5 > $SRC/results/central/Sherpa_3fractionfit_ee_top.log 2>&1 &
-
-RunFitterOnFractions.exe -M Migrad -d $SRC/Sherpa_mumu.root  -c $SRC/results/environments/Sherpa/BinnedEMLFraction.1o3.Sherpa_mumu_top.env -D "Data" -T "Zjets_b,Zjets_l,TTbar,Zjets_c" -o Sherpa_3fractionfit_mumu_top -r 5 > $SRC/results/central/Sherpa_3fractionfit_mumu_top.log 2>&1 &
-
-RunFitterOnFractions.exe -M Migrad -d $SRC/Sherpa_combined.root  -c $SRC/results/environments/Sherpa/BinnedEMLFraction.1o3.Sherpa_joined_top.env -D "Data_joined" -T "Zjets_b_joined,Zjets_l_joined,TTbar_joined,Zjets_c_joined" -o Sherpa_3fractionfit_joined_top -r 5 > $SRC/results/central/Sherpa_3fractionfit_joined_top.log 2>&1 &
-
--- results/central/do_3normalisations_top.sh
+++
@@ -1,5 +0,0 @@
-RunFitter.exe -M Migrad -d $SRC/Sherpa_ee.root  -c $SRC/results/environments/Sherpa/BinnedEML.4params.Sherpa_ee_top.env -D "Data" -T "Zjets_b,Zjets_c,Zjets_l,TTbar" -o Sherpa_3Normalisations_ee_top -r 5 > $SRC/results/central/Sherpa_3Normalisations_ee_top.log 2>&1 &
-
-RunFitter.exe -M Migrad -d $SRC/Sherpa_mumu.root  -c $SRC/results/environments/Sherpa/BinnedEML.4params.Sherpa_mumu_top.env -D "Data" -T "Zjets_b,Zjets_c,Zjets_l,TTbar" -o Sherpa_3Normalisations_mumu_top -r 5 > $SRC/results/central/Sherpa_3Normalisations_mumu_top.log 2>&1 &
-
-RunFitter.exe -M Migrad -d $SRC/Sherpa_combined.root  -c $SRC/results/environments/Sherpa/BinnedEML.4params.Sherpa_joined_top.env -D "Data_joined" -T "Zjets_b_joined,Zjets_c_joined,Zjets_l_joined,TTbar_joined" -o Sherpa_3Normalisations_joined_top -r 5 > $SRC/results/central/Sherpa_3Normalisations_joined_top.log 2>&1 &
