6037877d26d2e1f5127aceb302dcd0b1f1d74d99
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-19 15:38:57 +0100

    sv0 systematics files created but not yet working (top systematics are missing)

---
+++ bundles/sherpa/inclusive/systematics/SV0_50/createScripts.sh
@@ -0,0 +1,1 @@
+for item in `find . -type f -name "bundle*cfg"|grep -v template|sed -e s/.*_sumSV12mass_(.*).cfg.*/1/|sort`;do cp bundleIP3DSV1_70_allJets_sumSV12mass_template.cfg bundleIP3DSV1_70_allJets_sumSV12mass_${item}.cfg;sed -i -e "s/_JER_/_${item}_/g" bundleIP3DSV1_70_allJets_sumSV12mass_${item}.cfg;done
