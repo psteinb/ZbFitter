90bae4e4906e52049696a8723c00143cba87fd01
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2011-12-20 13:05:01 +0100

    updated createScript for IP3DSV1_70 systematics

--- results/systematics/sherpa/IP3DSV1_70/inclusive/do_SherpaIP3DSV1_70_3norm_top.sh
+++ results/systematics/sherpa/IP3DSV1_70/inclusive/do_SherpaIP3DSV1_70_3norm_top.sh
@@ -1,18 +1,12 @@
 
 echo "using input files from $SRC/bundles/sherpa/inclusive/systematics/IP3DSV1_70"
 
-if [[ $# > 0 ]];then
+if [[ -n ${1} ]];then
     
-    for file in $*;done
-    ENVFILE=`basename $file`
-    ENVFILE=${ENVFILE/Sherpa_/}
-    ENVFILE=${ENVFILE/_3norm_top.env/}
-    DATAFILE=$SRC/bundles/sherpa/inclusive/systematics/IP3DSV1_70/*${ENVFILE}
-    if [[ -f $DATAFILE ]];then
-	echo "using "
-	echo -e "\tinput:\t $DATAFILE"
-	echo -e "\tenv:  \t $ENVFILE"
-	RunFitter.exe -M Migrad -d $DATAFILE  -c $ENVFILE -D "Data" -T "TrueB_varied,TrueC_varied,TrueL_varied,Top_varied" -o Inclusive_${ENVFILE/.env/} -r 5 > `pwd`/${ENVFILE/.env/.log} 2>&1 &
+    for file in {AllFlavorsBTagEffDown,AllFlavorsBTagEffUp,HeavyFlavorBTagEffDown,HeavyFlavorBTagEffUp,JER,JESDOWN,JESUP,LightBTagEffDown,LightBTagEffUp,MuonEff,MuonPt};
+    do
+	ENVFILE=Sherpa_bundleIP3DSV1_70_allJets_sumSV12mass_${file}_3norm_top.env
+	RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/systematics/IP3DSV1_70/bundleIP3DSV1_70_allJets_sumSV12mass_${file}.root -c  -D "Data" -T "TrueB_varied,TrueC_varied,TrueL_varied,Top_varied" -o Inclusive_${ENVFILE/.env/} -r 5 > `pwd`/${ENVFILE/.env/.log} 2>&1 &
     else
 	echo "inputfile not found $DATAFILE"
     fi
--- results/systematics/sherpa/IP3DSV1_70/inclusive/prepare.sh
+++ results/systematics/sherpa/IP3DSV1_70/inclusive/prepare.sh
@@ -4,7 +4,7 @@ if [[ -n $1 ]];then
     for file in $*;do
 	FILENAME=`basename $file`
 	echo "python $SRC/python/printEnvFile.py -f $file -3 -N 'TrueB_varied,TrueC_varied,TrueL_varied,Top_varied'"
-	python $SRC/python/printEnvFile.py -f $file -3 -N "TrueB_varied,TrueC_varied,TrueL_varied,Top_varied"|egrep "^[A-Za-z]" > Sherpa_${FILENAME/.cfg/}_3norm_top.env
+	python $SRC/python/printEnvFile.py -f $file -3 -N "TrueB_varied,TrueC_varied,TrueL_varied,Top_varied"|egrep "^[A-Za-z]" > Sherpa_${FILENAME/.root/}_3norm_top.env
     done
 else
     echo "no input files given"
