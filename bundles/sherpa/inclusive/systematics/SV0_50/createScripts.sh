ddaca29cb404b5bed10df8a47b11982ef1aca6a8
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-01-04 15:21:19 +0100

    alpgen sv0 central fits done

--- bundles/sherpa/inclusive/systematics/SV0_50/createScripts.sh
+++ bundles/sherpa/inclusive/systematics/SV0_50/createScripts.sh
@@ -1,6 +1,9 @@
 
-for item in `find ${PWD} -name "bundle*cfg"|grep -v template|sed -e 's/.*_sumSV0mass_\(.*\)\.cfg.*/\1/'|sort`;do 
+for item in `find ${PWD} -type f -name "bundle*cfg"|grep -v template|sed -e 's/.*_sumSV0mass_\(.*\)\.cfg.*/\1/'|sort`;do 
     echo $item
+    cp -v customSV0_50_allJets_sumSV0mass_template.cfg customSV0_50_allJets_sumSV0mass_${item}.cfg
+    sed -i -e "s/_JER_/_${item}_/g" customSV0_50_allJets_sumSV0mass_${item}.cfg
     cp -v bundleSV0_50_allJets_sumSV0mass_template.cfg bundleSV0_50_allJets_sumSV0mass_${item}.cfg
     sed -i -e "s/_JER_/_${item}_/g" bundleSV0_50_allJets_sumSV0mass_${item}.cfg
+
 done
--- results/central/alpgen/SV0/inclusive/prepare.sh
+++ results/central/alpgen/SV0/inclusive/prepare.sh
@@ -1,2 +1,2 @@
-python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/AlpgenSV0_allJets_sumSV0mass.root -3|egrep "^[A-Za-z]" > AlpgenSV0_3norm_top_allJets_sumSV0mass.env
-python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/AlpgenSV0_allJets_sumSV0mass.root -2|egrep "^[A-Za-z]" > AlpgenSV0_2norm_fixedLight_fixedTop_sumSV1mass.env 
+python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/bundleSV0_allJets_sumSV0mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > AlpgenSV0_3norm_top_allJets_sumSV0mass.env
+python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/bundleSV0_allJets_sumSV0mass.root -3|egrep "^[A-Za-z]" > AlpgenSV0_3norm_background_allJets_sumSV0mass.env
