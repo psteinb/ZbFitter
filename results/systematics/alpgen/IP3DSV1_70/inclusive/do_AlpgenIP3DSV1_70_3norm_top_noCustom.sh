9962f1044c1b50336d379e6231cd62c943b349f4
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-01-04 12:41:39 +0100

    updated alpgen IP3DSV1X systematics fits

---
+++ results/systematics/alpgen/IP3DSV1_70/inclusive/do_AlpgenIP3DSV1_70_3norm_top_noCustom.sh
@@ -0,0 +1,13 @@
+
+echo "using input files from $SRC/bundles/alpgen/inclusive/systematics/IP3DSV1_70"
+
+    for file in {AllFlavorsBTagEffDown,AllFlavorsBTagEffUp,HeavyFlavorBTagEffDown,HeavyFlavorBTagEffUp,JER,JESDOWN,JESUP,LightBTagEffDown,LightBTagEffUp,MuonEff,MuonPt};
+    do
+	ENVFILE=Alpgen_bundleIP3DSV1_70_allJets_sumSV12mass_${file}_3norm_top.env
+	set -x
+	RunFitter.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/systematics/IP3DSV1_70/bundleIP3DSV1_70_allJets_sumSV12mass_${file}.root -c ${ENVFILE} -D "Data" -T "TrueB_varied,TrueC_varied,TrueL_varied,Top_varied" -o InclusiveData_${ENVFILE/.env/} -r 5 > `pwd`/${ENVFILE/.env/.log} 2>&1 &
+	RunFitter.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/systematics/IP3DSV1_70/bundleIP3DSV1_70_allJets_sumSV12mass_${file}.root -c ${ENVFILE} -D "PseudoData" -T "TrueB,TrueC,TrueL,Top" -o InclusivePseudoData_${ENVFILE/.env/} -r 5 > `pwd`/raw_${ENVFILE/.env/.log} 2>&1 &
+	set +x
+    done
+
+
