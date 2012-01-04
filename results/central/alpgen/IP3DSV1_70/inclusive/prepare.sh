aaebfeb36a94a4473bd7ed36911dc050a58bff8e
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-01-04 11:19:27 +0100

    updated alpgen systematics bundles

--- results/central/alpgen/IP3DSV1_70/inclusive/prepare.sh
+++ results/central/alpgen/IP3DSV1_70/inclusive/prepare.sh
@@ -1,2 +1,2 @@
-python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/AlpgenIP3DSV1_70_allJets_sumSV12mass.root -3|egrep "^[A-Za-z]" > AlpgenIP3DSV1_70_3norm_top_sumSV12mass.env
-python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/AlpgenIP3DSV1_70_allJets_sumSV12mass.root -2|egrep "^[A-Za-z]" > AlpgenIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass.env 
+python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/AlpgenIP3DSV1_70_allJets_sumSV12mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > AlpgenIP3DSV1_70_3norm_top_sumSV12mass.env
+python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/AlpgenIP3DSV1_70_allJets_sumSV12mass.root -3|egrep "^[A-Za-z]" > AlpgenIP3DSV1_70_3norm_background_sumSV12mass.env 
