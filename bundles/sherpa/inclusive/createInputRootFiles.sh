71154dc736130b8a1446e3524313cb566e7a39a8
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-01-04 15:42:45 +0100

    updated sherpa/alpgen sv0 systematics bundles

--- bundles/sherpa/inclusive/createInputRootFiles.sh
+++ bundles/sherpa/inclusive/createInputRootFiles.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-python $SRC/python/bundleHistos.py -f `pwd`/bundleSV0_allJets_sumSV0mass.cfg -o SherpaSV0_sumSV0mass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_70_allJets_sumSV12mass.cfg -o SherpaIP3DSV1_70_sumSV12mass.root
-python $SRC/python/bundleHistos.py -f `pwd`/bundleIP3DSV1_80_allJets_sumSV12mass.cfg -o SherpaIP3DSV1_80_sumSV12mass.root
-
-python $SRC/python/bundleHistos.py -f `pwd`/customSV0_allJets_sumSV0mass.cfg -o SherpaSV0_sumSV0mass_custom.root
-python $SRC/python/bundleHistos.py -f `pwd`/customIP3DSV1_70_allJets_sumSV12mass.cfg -o SherpaIP3DSV1_70_sumSV12mass_custom.root
-python $SRC/python/bundleHistos.py -f `pwd`/customIP3DSV1_80_allJets_sumSV12mass.cfg -o SherpaIP3DSV1_80_sumSV12mass_custom.root
-
+for file in $*;
+do 
+    echo
+    if [[ $file == *cfg ]];then
+	echo "processing ${file}"
+	python $SRC/python/bundleHistos.py -f `pwd`/$file -o ${file/cfg/root} > ${file/.cfg/.log} 2>&1 &
+    else
+	echo "cannot process ${file}"
+    fi
+done
