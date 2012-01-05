e6ef5891c19c5a676665847a2a238f2cf1b6966d
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-01-05 13:29:59 +0100

    updated systematics results for sherpa

--- results/systematics/sherpa/IP3DSV1_70/prepare.sh
+++
@@ -1,2 +0,0 @@
-python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_70_sumSV12mass.root -3|egrep "^[A-Za-z]" > SherpaIP3DSV1_70_3norm_top_sumSV12mass.env
-python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/SherpaIP3DSV1_70_sumSV12mass.root -2|egrep "^[A-Za-z]" > SherpaIP3DSV1_70_2norm_fixedLight_fixedTop_sumSV1mass.env 
