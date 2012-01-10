51d507a2186e077c8ec02c46693d38f3edc863d9
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-01-10 14:43:06 +0100

    updated all sherpa systematics custom bundles

--- bundles/sherpa/inclusive/systematics/IP3DSV1_80/createScripts.sh
+++ bundles/sherpa/inclusive/systematics/IP3DSV1_80/createScripts.sh
@@ -3,4 +3,7 @@ for item in `find ${PWD} -name "bundle*cfg"|grep -v template|sed -e 's/.*_sumSV1
     echo $item
     cp -v bundleIP3DSV1_80_allJets_sumSV12mass_template.cfg bundleIP3DSV1_80_allJets_sumSV12mass_${item}.cfg;
     sed -i -e "s/_JER_/_${item}_/g" bundleIP3DSV1_80_allJets_sumSV12mass_${item}.cfg;
+
+    cp -v customIP3DSV1_80_allJets_sumSV12mass_template.cfg customIP3DSV1_80_allJets_sumSV12mass_${item}.cfg;
+    sed -i -e "s/_JER_/_${item}_/g" customIP3DSV1_80_allJets_sumSV12mass_${item}.cfg;
 done
