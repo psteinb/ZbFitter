f4d62c849b80d0125e3bf0f85dc6befed06b3b51
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-01-10 14:58:26 +0100

    added scripts for custom fit central results in alpgen

--- results/central/alpgen/SV0/inclusive/prepare.sh
+++ results/central/alpgen/SV0/inclusive/prepare.sh
@@ -1,2 +1,5 @@
 python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/bundleSV0_allJets_sumSV0mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > AlpgenSV0_3norm_top_allJets_sumSV0mass.env
 python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/bundleSV0_allJets_sumSV0mass.root -3|egrep "^[A-Za-z]" > AlpgenSV0_3norm_background_allJets_sumSV0mass.env
+
+python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > AlpgenSV0_3norm_top_custom_sumSV0mass.env
+python $SRC/python/printEnvFile.py -f $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root -3|egrep "^[A-Za-z]" > AlpgenSV0_3norm_background_custom_sumSV0mass.env
