6183296f06de91f23784fd86ae30cf07b79dd341
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-02-03 16:26:46 +0100

    added sherpa IP3DSV1_80 linearity tests

---
+++ results/linear/sherpa/IP3DSV1_80/do_SherpaIP3dSV1_linear.sh
@@ -0,0 +1,12 @@
+
+for item in $*;
+do
+if [[ $item != *.env ]];then
+    continue
+fi
+envId=`echo $item|sed -e 's/.*custom\(.*\)\.env/\1/'`
+echo "processing $item (id: ${envId})"
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/linearity/customSV0_allJets_sumSV0mass_TrueB_scaled.root  -c $item -D "Data" -T "TrueB${envId},TrueC,TrueL,Background" -o ${item/.env/} -r 5 -i 15000  > ${item/.env/.log} 2>&1 &
+done
+#RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/SherpaSV0.root  -c SherpaSV0_2norm_fixedLight_fixedTop.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop -r 5 -i 15000 > $SRC/results/central/SherpaSV0_2norm_fixedLight_fixedTop.log 2>&1 &
+
