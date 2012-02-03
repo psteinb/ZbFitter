435c3d25f0bf9b51922c5ab3ab97d95e84566340
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-02-03 16:29:27 +0100

    sherpa linearity finished

--- results/linear/sherpa/IP3DSV1_80/do_SherpaIP3dSV1_linear.sh
+++ results/linear/sherpa/IP3DSV1_80/do_SherpaIP3dSV1_linear.sh
@@ -6,7 +6,7 @@ if [[ $item != *.env ]];then
 fi
 envId=`echo $item|sed -e 's/.*custom\(.*\)\.env/\1/'`
 echo "processing $item (id: ${envId})"
-RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/linearity/customSV0_allJets_sumSV0mass_TrueB_scaled.root  -c $item -D "Data" -T "TrueB${envId},TrueC,TrueL,Background" -o ${item/.env/} -r 5 -i 15000  > ${item/.env/.log} 2>&1 &
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/linearity/customIP3DSV1_80_allJets_sumSV12mass_TrueB_scaled.root  -c $item -D "Data" -T "TrueB${envId},TrueC,TrueL,Background" -o ${item/.env/} -r 5 -i 15000  > ${item/.env/.log} 2>&1 &
 done
 #RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/sherpa/SherpaSV0.root  -c SherpaSV0_2norm_fixedLight_fixedTop.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_2norm_fixedLight_fixedTop -r 5 -i 15000 > $SRC/results/central/SherpaSV0_2norm_fixedLight_fixedTop.log 2>&1 &
 
