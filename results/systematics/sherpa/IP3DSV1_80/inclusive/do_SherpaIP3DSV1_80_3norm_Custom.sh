d05bc4211bdc64aa15bdc8bdc52be551ab40dc1f
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-02-16 15:32:06 +0100

    added varied PseudoData to systematics bundles

--- results/systematics/sherpa/IP3DSV1_80/inclusive/do_SherpaIP3DSV1_80_3norm_Custom.sh
+++ results/systematics/sherpa/IP3DSV1_80/inclusive/do_SherpaIP3DSV1_80_3norm_Custom.sh
@@ -6,7 +6,7 @@ echo "using input files from $SRC/bundles/sherpa/inclusive/systematics/IP3DSV1_8
 	ENVFILE=Sherpa_customIP3DSV1_80_allJets_sumSV12mass_${file}_3norm_background.env
 	set -x
 	TOPFILE=InclusivePseudoData_${ENVFILE/.env/}
-	#RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/systematics/IP3DSV1_80/bundleIP3DSV1_80_allJets_sumSV12mass_${file}.root -c ${ENVFILE} -D "PseudoData" -T "TrueB_varied,TrueC_varied,TrueL_varied,Top_varied" -o $TOPFILE -r 5 > `pwd`/$TOPFILE.log 2>&1 &
+	RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/systematics/IP3DSV1_80/bundleIP3DSV1_80_allJets_sumSV12mass_${file}.root -c Sherpa_3norm_background_templNominal.env -D "PseudoData_varied" -T "TrueB,TrueC,TrueL,Top" -o Sherpa_3norm_background_templNominal_${file} -r 5 > `pwd`/Sherpa_3norm_background_templNominal_${file}.log 2>&1 &
 	BACKFILE=${TOPFILE//top/background}
 	RunFitter.exe -M Migrad -d $SRC/bundles/sherpa/inclusive/systematics/IP3DSV1_80/customIP3DSV1_80_allJets_sumSV12mass_${file}.root -c ${ENVFILE/top/background} -D "PseudoData" -T "TrueB_varied,TrueC_varied,TrueL_varied,Background_varied" -o $BACKFILE -r 5 > `pwd`/$BACKFILE.log 2>&1 &
 
--- results/systematics/sherpa/IP3DSV1_80/inclusive/prepare.sh
+++ results/systematics/sherpa/IP3DSV1_80/inclusive/prepare.sh
@@ -19,7 +19,8 @@ for file in $TARGETS;do
     # echo "python $SRC/python/printEnvFile.py -f $file -3 -N 'TrueB_varied,TrueC_varied,TrueL_varied,Top_varied'"
     # python $SRC/python/printEnvFile.py -f $file -3 -N "TrueB_varied,TrueC_varied,TrueL_varied,Top_varied"|egrep "^[A-Za-z]" > Sherpa_${FILENAME/.root/}_3norm_top.env
     echo "python $SRC/python/printEnvFile.py -f $file -3 -N 'TrueB_varied,TrueC_varied,TrueL_varied,Background_varied'"
-    python $SRC/python/printEnvFile.py -f $file -3 -N "TrueB_varied,TrueC_varied,TrueL_varied,Background_varied"|egrep "^[A-Za-z]" > Sherpa_${FILENAME/.root/}_3norm_background.env
-    
+    python $SRC/python/printEnvFile.py -f $file -3 -N "TrueB_varied,TrueC_varied,TrueL_varied,Background_varied"|egrep "^[A-Za-z]" > Sherpa_${FILENAME/.root/}_3norm_background_templVaried.env
+
 done
 
+python $SRC/python/printEnvFile.py -f /home/psteinb/development/sandbox/fitter/bundles/sherpa/inclusive/customIP3DSV1_80_allJets_sumSV12mass.root -3 -N "TrueB,TrueC,TrueL,Background"|egrep "^[A-Za-z]" > Sherpa_3norm_background_templNominal.env
