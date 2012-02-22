44b0983a3ee3860661dc0f4a5c8c03f2383453e9
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-02-22 21:34:38 +0100

    updated setups for ensemble tests

--- results/ensemble/sherpa/SV0/prepare.sh
+++ results/ensemble/sherpa/SV0/prepare.sh
@@ -1,5 +1,7 @@
-python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/bundleSV0_allJets_sumSV0mass.root -3|egrep "^[A-Za-z]" > SherpaSV0_3norm_background_sumSV0mass.env
-python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/bundleSV0_allJets_sumSV0mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > SherpaSV0_3norm_top_sumSV0mass.env
+#python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/bundleSV0_allJets_sumSV0mass.root -3|egrep "^[A-Za-z]" > SherpaSV0_3norm_background_sumSV0mass.env
+#python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/bundleSV0_allJets_sumSV0mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > SherpaSV0_3norm_top_sumSV0mass.env
 
 python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/customSV0_allJets_sumSV0mass.root -3|egrep "^[A-Za-z]" > SherpaSV0_3norm_background_sumSV0mass_custom.env
-python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/customSV0_allJets_sumSV0mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > SherpaSV0_3norm_top_sumSV0mass_custom.env
+#python $SRC/python/printEnvFile.py -f $SRC/bundles/sherpa/inclusive/customSV0_allJets_sumSV0mass.root -3 -N "TrueB,TrueC,TrueL,Top"|egrep "^[A-Za-z]" > SherpaSV0_3norm_top_sumSV0mass_custom.env
+cp SherpaSV0_3norm_background_sumSV0mass_custom.env SherpaSV0_3free_background_sumSV0mass_custom.env
+sed -i -e '/low/s/^/#/g' SherpaSV0_3free_background_sumSV0mass_custom.env
