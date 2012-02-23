14cbea8d846dfc11e0a118b06a784e4fc23abcce
Peter Steinbach <P.Steinbach@physik.tu-dresden.de>
2012-02-23 14:04:50 +0100

    added license, reversed definition of bias to comply with textbookst

--- results/ensemble/alpgen/SV0/do_AlpgenSV0.sh
+++ results/ensemble/alpgen/SV0/do_AlpgenSV0.sh
@@ -1,13 +1,13 @@
-#RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3norm_top_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o AlpgenSV0_3norm_top -r 5 -i 50000  > $SRC/results/central/AlpgenSV0_3norm_top.log 2>&1 &
+##RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3norm_top_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Top" -o AlpgenSV0_3norm_top -r 5 -i 50000  > $SRC/results/central/AlpgenSV0_3norm_top.log 2>&1 &
 
-RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3norm_background_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Background" -o AlpgenSV0_3norm_background -r 5 -i 50000  > $SRC/results/central/AlpgenSV0_3norm_background.log 2>&1 &
+#RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3norm_background_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Background" -o AlpgenSV0_3norm_background -r 5 -i 50000  > $SRC/results/central/AlpgenSV0_3norm_background.log 2>&1 &
 
 RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3free_background_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Background" -o AlpgenSV0_3free_background -r 5 -i 50000  > $SRC/results/central/AlpgenSV0_3free_background.log 2>&1 &
 
 
-RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3norm_background_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Background" -o AlpgenSV0_3norm_background_x200 -r 5 -i 10000 -P 200  > $SRC/results/central/AlpgenSV0_3norm_background_x200.log 2>&1 &
+#RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3norm_background_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Background" -o AlpgenSV0_3norm_background_x150 -r 5 -i 25000 -P 150  > $SRC/results/central/AlpgenSV0_3norm_background_x150.log 2>&1 &
 
-RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3free_background_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Background" -o AlpgenSV0_3free_background_x200 -r 5 -i 10000 -P 200  > $SRC/results/central/AlpgenSV0_3free_background_x200.log 2>&1 &
+RunPseudoExperiments.exe -M Migrad -d $SRC/bundles/alpgen/inclusive/customSV0_allJets_sumSV0mass.root  -c AlpgenSV0_3free_background_sumSV0mass_custom.env -D "Data" -T "TrueB,TrueC,TrueL,Background" -o AlpgenSV0_3free_background_x150 -r 5 -i 25000 -P 150  > $SRC/results/central/AlpgenSV0_3free_background_x150.log 2>&1 &
 
 
 
